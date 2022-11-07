19736ab372724c0c620bdbaa1afe194828c9c928
Marco Almeida <markilha@gmail.com>
2022-11-06 22:47:46 -0300

    Ajustando o teste

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const estilos = {
-  normal: "normal",
-  monetario: "monetario",
-  porcentagem: "porcentagem",
-  agraria: "agraria",
-};
+function calculaDistancia(lat1, lon1, lat2, lon2) {
+  var deg2rad = 0.017453292519943295; // === Math.PI / 180
+  var cos = Math.cos;
+  lat1 *= deg2rad;
+  lon1 *= deg2rad;
+  lat2 *= deg2rad;
+  lon2 *= deg2rad;
+  var diam = 12742; // Diameter of the earth in km (2 * 6371)
+  var dLat = lat2 - lat1;
+  var dLon = lon2 - lon1;
+  var a = (1 - cos(dLat) + (1 - cos(dLon)) * cos(lat1) * cos(lat2)) / 2;
+
+  return diam * Math.asin(Math.sqrt(a)) * 1000;
+}
 
 module.exports = {
-  porExtenso,
-  estilo: estilos,
+  calculaDistancia,
 };
-
-function porExtenso(numero, estilo = estilos.normal) {
-  if (!estilos[estilo])
-    throw new Error(
-      `Estilo "${estilo}" inválido. Possíveis valores: normal, monetario, porcentagem`
-    );
-
-  return require("./lib/estilos/" + estilo)(numero);
-}
